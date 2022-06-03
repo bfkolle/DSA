3e1bd8ef14928e3f2c88b8e5c4612f42c13ea797
Brandon Kolle <bfkolle@gmail.com>
2022-06-03 15:24:55 -0400

    Add additional peek test

--- tests/test_stacksandqueues.py
+++ tests/test_stacksandqueues.py
@@ -12,9 +12,13 @@ def getteststack():
 
     return stack
 
-def test_whenstackempty_peek_returnsnone():
+def test_when_stackempty_peek_returnsnone():
     stack = getemptyteststack()
     assert stack.peek() == None
+
+def test_peek_returns_topstackvalue():
+    stack = getteststack()
+    assert stack._list[0] == stack.peek()
 
 def test_valueontop_is_lastpushedvalue():
     stack = getteststack()
