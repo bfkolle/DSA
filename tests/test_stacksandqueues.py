f7ea80a2eae9364d4b7bcfb7f625601216e9d11b
Brandon Kolle <bfkolle@gmail.com>
2022-06-03 15:19:27 -0400

    Add peek test

--- tests/test_stacksandqueues.py
+++ tests/test_stacksandqueues.py
@@ -1,6 +1,9 @@
 import re
 from stacksandqueues.stack import Stack
 
+def getemptyteststack():
+    return Stack()
+
 def getteststack():
     stack = Stack()
     stack.push("Hello")
@@ -9,6 +12,9 @@ def getteststack():
 
     return stack
 
+def test_whenstackempty_peek_returnsnone():
+    stack = getemptyteststack()
+    assert stack.peek() == None
 
 def test_valueontop_is_lastpushedvalue():
     stack = getteststack()
