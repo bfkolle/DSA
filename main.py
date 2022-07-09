44676b15c37acd5c6188aa64d4e1a24a85e2d999
Brandon Kolle <bfkolle@gmail.com>
2022-07-09 12:56:16 -0400

    Yeet manual tests in maine file

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 from stacksandqueues.stack import Stack
 
 def main():
-    #todo: make unit tests
-    stack = Stack()
-
-    stack.peek()
-
-    print("Value at top after three pushes:", stack._list[0])
-
-    stack.pop()
-    print("Value at top after one pop:", stack._list[0])
-
-    print("Is stack full with two values:", stack.isFull())
-
-    stack.pop()
-    stack.pop()
-    print("Stack is empty after removing all values:", stack.isEmpty())
+    print("Data Structures!")
 
 if __name__ == "__main__":
     main()
